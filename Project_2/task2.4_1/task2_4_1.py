f222f52cbda7692ab3df2e0b1e429698f28ee3a8
sezerse <selinsezer@gmail.com>
2019-05-23 18:29:36 +0200

    bad solution for 2.4 -needs refactoring

--- Project_2/task2.4_1/task2_4_1.py
+++ Project_2/task2.4_1/task2_4_1.py
@@ -1,5 +1,7 @@
 import itertools
 import numpy as np
+import numpy.linalg as la
+from itertools import chain, combinations
 
 
 class CellularAutomation:
@@ -7,24 +9,58 @@ class CellularAutomation:
         self.setup()
 
     def setup(self):
-        input_perm = list(itertools.product([1, -1], repeat=3))
-        data_type = np.dtype('int,int,int')
-        design_matrix_X = np.array(input_perm, dtype=data_type)
-        rule_110_y = list('{0:08b}'.format(110)) #TODO change these to -1,+1
-        rule_126_y = list('{0:08b}'.format(126))
+        self.input = np.array(list(list(tup) for tup in itertools.product([-1, 1], repeat=3)))
+        rule_110_y = np.array(list('{0:08b}'.format(110))).astype('int')
+        self.rule_110_y = self.correct_y_values(rule_110_y)
+        rule_126_y = np.array(list('{0:08b}'.format(126))).astype('int')
+        self.rule_126_y = self.correct_y_values(rule_126_y)
+
+    def correct_y_values(self, y):
+        return 2*y-1
 
     def subtask_1(self):
-        pass
+        X_T = self.input
 
-    def subtask_2(self):
-        pass
+        y110 = self.rule_110_y
+        w110 = la.lstsq(X_T, y110)[0]
+        yhat_110 = np.dot(X_T, w110)
+        print "Rule 110: \n y values are: {} \n y^values are: {}".format(y110.astype('float'), yhat_110)
+
+        y126 = self.rule_126_y
+        w126 = la.lstsq(X_T, y126)[0]
+        yhat_126 = np.dot(X_T, w126)
+        print "Rule 126: \n y values are: {} \n y^values are: {}".format(y126.astype('float'), yhat_126)
+        #TODO refactor
+
+    def subtask_2(self, var_list):
+        power_set = self.get_powerset(var_list)
+        for index in range(len(power_set)):
+            sublist_len = len(power_set[index])
+            if sublist_len == 0:
+                power_set[index] = 1
+            elif sublist_len == 1:
+                power_set[index] = power_set[index][0]
+            else:
+                power_set[index] = np.prod(power_set[index])
+        return power_set
+
+    def get_powerset(self, set):
+        return list(list(tup) for tup in chain.from_iterable(combinations(set, n) for n in range(len(set) + 1)))
 
     def subtask_3(self):
-        pass
+        result_list = list()
+        for input_row in self.input:
+            result_list.append(ca.subtask_2(input_row.tolist()))
+        self.input = np.array(result_list)
+        self.subtask_1()
 
 
 if __name__ == "__main__":
     ca = CellularAutomation()
+    print "---------------------------------------------"
+    print "Subtask 1"
     ca.subtask_1()
-    ca.subtask_2()
+    print "---------------------------------------------"
+    print "Subtask 3"
     ca.subtask_3()
+    print "---------------------------------------------"
